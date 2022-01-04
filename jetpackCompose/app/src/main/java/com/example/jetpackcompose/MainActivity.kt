68803fe038b71eceb4109227af8cab3aebc94d26
isilay-subasi <isil.subasi19@gmail.com>
2022-01-04 20:40:50 +0300

    Column,Rox,Box

--- jetpackCompose/app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ jetpackCompose/app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -3,18 +3,22 @@ package com.example.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+           MainScreen()
         }
 
        // testFunction(5,::myTestFunction)
@@ -37,14 +41,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen(){
+
+    //Column, Row, Box
+
+    Column() {
+        // Text(text = "Hello $name!",color = Color.Magenta)
+        Text(text = "Hello Android",color = Color.Magenta,
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(text = "Hello World",color = Color.Magenta,
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+
+
+
+
 }
+
+
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpackComposeTheme {
-        Greeting("Android")
-    }
+   MainScreen()
 }
