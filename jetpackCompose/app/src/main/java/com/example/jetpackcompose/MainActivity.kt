8a57b2b664e844cd6756a4d5954664bb6540978a
isilay-subasi <isil.subasi19@gmail.com>
2022-01-04 17:11:34 +0300

    Lambda

---
+++ jetpackCompose/app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+
+       // testFunction(5,::myTestFunction)
+
+        testFunction(6){
+            myTestFunction()
+        }
+    }
+
+    fun testFunction(int : Int , myTestFunction : () -> Unit){
+
+    }
+
+    fun myTestFunction(){
+        println("test")
+    }
+
+
+
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeTheme {
+        Greeting("Android")
+    }
+}
