39dcc7570b5ee00af29db4af5afec28a7a3a7f76
isilay-subasi <isil.subasi19@gmail.com>
2022-01-04 21:19:41 +0300

    Modifier

--- jetpackCompose/app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ jetpackCompose/app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
@@ -47,7 +52,12 @@ fun MainScreen(){
 
     Column() {
         // Text(text = "Hello $name!",color = Color.Magenta)
-        Text(text = "Hello Android",color = Color.Magenta,
+        Text(modifier = Modifier.background(color = Color.Cyan)
+            .padding(10.dp)
+            .clickable {
+                       println("Hello World clicked")
+            }
+            ,text = "Hello Android",color = Color.Magenta,
             fontSize = 25.sp,
             fontWeight = FontWeight.Bold
         )
